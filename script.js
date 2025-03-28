c7a234e5bb9d32a833ea103ccc1f5a7f860c0d92
Debayan <73572191+Debayan-creator@users.noreply.github.com>
2025-03-28 16:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery img');
+    images.forEach(img => {
+        img.addEventListener('click', function() {
+            alert('You clicked on: ' + this.alt);
+        });
+    });
+});
+
